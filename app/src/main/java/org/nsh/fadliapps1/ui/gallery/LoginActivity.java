3a6933639a80a29d26f0f3caa823476208c7fede
AKMAL <emailgit>
2021-04-03 20:55:00 +0700

    Tugas 1

---
+++ app/src/main/java/org/nsh/fadliapps1/ui/gallery/LoginActivity.java
@@ -0,0 +1,50 @@
+package org.nsh.fadliapps1.ui.gallery;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.nsh.fadliapps1.R;
+
+public class LoginActivity  extends AppCompatActivity {
+    Button login;
+    EditText username,password;
+
+    protected void onCreate(Bundle saveInstanceState) {
+
+        super.onCreate(saveInstanceState);
+
+        setContentView(R.layout.login_relative);
+
+        login = (Button) findViewById(R.id.btnLogin);
+        username = (EditText) findViewById(R.id.etUsername);
+        password = (EditText) findViewById(R.id.Password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (password.getText().toString().toUpperCase().equals("FEBBY"))
+                {
+                    login_sucsess();
+                } else
+                {
+                    Toast.makeText(getApplicationContext(), "Salah Password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void login_sucsess() {
+        String user_login = username.getText().toString();
+        Intent i = new Intent(this,MainActivity.class);
+        //kirimkan usernamenya ke tampilan utama
+        i.putExtra("USERNAME",user_login);
+        startActivity(i);
+    }
+}
